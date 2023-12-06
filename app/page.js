0cb8dabe1760f1b8d7b67e1782e6b44e04cc315e
nvoers <nick-vanoers@hotmail.com>
2023-12-06 10:32:19 +0100

    rework hero, add content

--- app/page.js
+++ app/page.js
@@ -1,31 +1,69 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar.js";
+import Footer from "./components/footer/Footer.js";
 
 export default function Home() {
   return (
-    <main className="container mx-auto h-screen">
+    <>
       <Navbar />
-      <div className="w-full h-[70%] bg-gradient-to-b from-primary to-zinc-100 rounded-xl mt-8 p-8 flex justify-center">
-        <div className="w-[40%] flex flex-col place-content-center h-full py-8">
-          <p className="font-bold text-5xl pb-4">
-            All your tickets in one place
-          </p>
-          <div className="pt-4">
-            <div className="btn btn-secondary text-lg mr-6">Register</div>
-            <div className="btn btn-secondary text-lg">Login</div>
+      <div className="container mx-auto pt-8">
+        <div className="w-full bg-gradient-to-b from-primary to-zinc-100 rounded-xl px-8 py-28 flex justify-between">
+          <div className="w-1/2 flex flex-col place-content-center py-8 pl-24">
+            <p className="font-bold text-5xl pb-4 text-white w-fit">
+              All your tickets in one place
+            </p>
+            <div className="pt-4">
+              <div className="btn btn-secondary text-lg mr-6">Register</div>
+              <div className="btn btn-secondary text-lg">Login</div>
+            </div>
           </div>
-        </div>
-        <div className="w-[40%] flex flex-col place-content-center h-full py-8">
-          <div className="w-full h-[75%] relative">
-            <Image
-              fill
-              src="/ticket-icon.svg"
-              layout="fill"
-              objectFit="contain"
-            />
+          <div className="w-1/2 flex flex-col place-content-center py-8">
+            <div className="flex justify-center">
+              <Image
+                src="/ticket-icon.svg"
+                objectFit="contain"
+                width={300}
+                height={300}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </main>
+      <div className="container mx-auto pt-16">
+        <p className="text-5xl font-bold">
+          Ticketdeck: Your personal digital ticket wallet
+        </p>
+        <p className="pt-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim
+          lobortis scelerisque fermentum dui faucibus in. Tortor condimentum
+          lacinia quis vel eros donec. Pellentesque dignissim enim sit amet
+          venenatis. Lacus sed viverra tellus in hac habitasse platea dictumst
+          vestibulum.
+        </p>
+        <div className="flex w-full items-center justify-start space-x-4">
+          <div className="relative h-8 w-8 flex-none overflow-hidden rounded-full">
+            <Image
+              alt="Nick van Oers"
+              loading="lazy"
+              width="100"
+              height="100"
+              decoding="async"
+              data-nimg="1"
+              className="h-full w-full object-cover duration-700 ease-in-out scale-100 blur-0"
+              src="/nick.jpg"
+            />
+          </div>
+          <p className="ml-3 inline-block whitespace-nowrap align-middle text-sm font-semibold dark:text-white md:text-base">
+            Nick van Oers
+          </p>
+          <div className="h-6 border-l border-stone-600 dark:border-stone-400"></div>
+          <p className="m-auto my-5 w-10/12 text-sm font-light text-stone-500 dark:text-stone-400 md:text-base">
+            Dec 06, 2023
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
