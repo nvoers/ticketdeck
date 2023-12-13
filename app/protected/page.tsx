f8b4e74c9dbc9ae2ff5682395fe68adb8090e572
nvoers <nick-vanoers@hotmail.com>
2023-12-13 16:33:23 +0100

    start on homepage

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,5 +1,6 @@
 import SignOut from "@/components/sign-out";
 import Navbar from "@/components/navbar";
+import NextEvent from "@/components/next-event";
 
 export default function Home() {
 
@@ -7,6 +8,26 @@ export default function Home() {
     <>
     {/* @ts-expect-error Server Component */}
       <Navbar />
+      <div className="container mx-auto">
+        <NextEvent />
+        <div className="grid grid-cols-3  mt-8 gap-8">
+          <div className="card w-96 bg-gradient-to-b from-zinc-100 to-primary text-white">
+            <div className="card-body">
+              <h2 className="card-title">ADD TICKET</h2>
+            </div>
+          </div>
+          <div className="card w-96 bg-gradient-to-b from-zinc-100 to-primary text-white">
+            <div className="card-body">
+              <h2 className="card-title">VIEW TICKETS</h2>
+            </div>
+          </div>
+          <div className="card w-96 bg-gradient-to-b from-zinc-100 to-primary text-white">
+            <div className="card-body">
+              <h2 className="card-title">DERDE OPTIE</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ components/next-event.tsx
@@ -0,0 +1,9 @@
+
+export default function nextEvent() {
+
+  return (
+    <div className="bg-zinc-100 rounded-lg p-4 mt-4 drop-shadow-md">
+        <p className="text-4xl font-bold">Your next event: Eventname in concert at sea longname</p>
+    </div>
+  );
+}
