7ffc2dc84dedd11a37b6601e8398460f7eb3e588
nvoers <nick-vanoers@hotmail.com>
2023-12-13 15:32:13 +0100

    signout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,34 @@
 import Link from "next/link";
+import { getServerSession } from "next-auth/next";
+import SignOut from "./sign-out";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getServerSession();
+
     return (
       <>
         <div className="navbar container mx-auto">
           <div className="navbar-start"></div>
           <div className="navbar-center">
-            <a className="btn btn-ghost text-xl">Ticketdeck</a>
+            <Link
+            href="/"
+            prefetch={false} // workaround until https://github.com/vercel/vercel/pull/8978 is deployed
+            className="btn btn-ghost text-xl"
+          >
+            Ticketdeck
+          </Link>
           </div>
           <div className="navbar-end">
+            {session ? 
+            <SignOut /> :
           <Link
-            href="/login"
-            prefetch={false} // workaround until https://github.com/vercel/vercel/pull/8978 is deployed
-            className="btn btn-accent btn-outline"
-          >
-            Login
-          </Link>
+          href="/login"
+          prefetch={false} // workaround until https://github.com/vercel/vercel/pull/8978 is deployed
+          className="btn-accent btn btn-outline"
+        >
+          Login
+        </Link>}
+
           </div>
         </div>
       </>
