752844cb682260dd1e61f195c06eccff757d47a7
nvoers <nick-vanoers@hotmail.com>
2023-12-21 13:05:30 +0100

    change prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+	var prisma: PrismaClient | undefined;
 }
 
-let prisma;
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
-}
-
-export default prisma
+export default prisma;
